3dd0edfda9266a3da594d7603d1aa83b7a81b2b4
vggog <gluhovkich@gmail.com>
2022-09-02 03:25:30 +0500

    little refactor

--- app/counter.py
+++
@@ -1,6 +0,0 @@
-class LineCounter:
-
-    @staticmethod
-    def get_number_of_lines_in_file(file_path: str) -> int:
-        with open(file_path) as file:
-            return len(file.readlines())
--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
-from app.files import FilesInDirectory
-from app.counter import LineCounter
+from app.service import get_number_of_lines_in_file, get_file_path
 
 
 def main():
     total_line = 0
     total_files = 0
-    print(FilesInDirectory.get_dirs_in_current_dir())
-    for file in FilesInDirectory.get_all_files():
-        number_of_line = LineCounter.get_number_of_lines_in_file(file)
-        print(file, '->', number_of_line)
+    for file_path in get_file_path():
+        number_of_line = get_number_of_lines_in_file(file_path)
+        print(file_path, '->', number_of_line)
         total_line += number_of_line
         total_files += 1
 
-    print('=================================================================')
+    print('=' * 79)
     print('total files: ', total_files)
     print('total line: ', total_line)
-
